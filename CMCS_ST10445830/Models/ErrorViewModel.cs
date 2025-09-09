333ed3aa751bab0f19a59378eac976eecbdd8fc8
Jackson-Matthew <164759934+Jackson-Matthew@users.noreply.github.com>
2025-09-09 10:46:51 +0200

    First commit
    Initial setup of the project before adding GUI

---
+++ CMCS_ST10445830/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMCS_ST10445830.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
